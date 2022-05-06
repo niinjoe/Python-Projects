7c2f32ad148f714e088d92fb55fad96cd5596838
Joe <80960873+niinjoe@users.noreply.github.com>
2022-05-06 11:15:26 -0400

    removed old files & cleaning folders

--- Tools/formartter.py
+++ Tools/formartter.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 df = pd.read_excel("unformatted_data.xlsx")
 
-df["data"] = "HPM-" + df["data"].astype(str).str.zfill(5)
+df["data"] = "Name" + df["data"].astype(str).str.zfill(5)
 
 # df["data"] = df["data"].astype(str)
 # for _, num in df.iterrows():
 #     while len(num.values[0]) < 5:
 #         num.values[0] = "0" + num.values[0]
-#     num.values[0] = "HPM-" + num.values[0]
+#     num.values[0] = "Name" + num.values[0]
 
 print(df)
