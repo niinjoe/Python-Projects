99f4cb7b9721443b2e71189249ec1f77126dbddc
Joe <80960873+niinjoe@users.noreply.github.com>
2022-05-06 12:11:50 -0400

    moving folders & organizing

--- Challenges/py_challenge_4/column_combinator.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("marca_modelo.csv")
-
-df["Marca y Modelo"] = df["1"] + ' - ' + df["2"]
-
-print(df.head())
-
-df.to_csv("combinados.csv", index=False)
--- HPM/2022/Reportes Py/01/main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel("Reporte PCF.xlsx")
-
-df.drop_duplicates(subset="Patient Name", keep="first", inplace=True)
-
-df.to_excel("Reporte_dropduplicates.xlsx", index=False)
