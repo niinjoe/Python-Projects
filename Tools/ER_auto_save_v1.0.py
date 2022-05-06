9280f82d48ad63c48f9ec03bfeb3817db9a3cc0e
Joe <80960873+niinjoe@users.noreply.github.com>
2022-05-06 09:48:03 -0400

    elden ring auto saver creation

--- Tools/ER_auto_save_v1.0.py
+++ Tools/ER_auto_save_v1.0.py
@@ -4,13 +4,13 @@ import os
 
 # src = steam save file path
 # dest = local backup path
-save_file_src = r"C:\Users\jserrano\OneDrive - HPM Foundation, Inc\Desktop\Test Folder"
-save_file_dest = r"C:\Users\jserrano\OneDrive - HPM Foundation, Inc\Desktop\Test Folder Copied"
+save_file_src = r"C:\Path\to\original save file"
+save_file_dest = r"C:\Path\to\copied save file"
 
 shutil.copytree(save_file_src, save_file_dest, dirs_exist_ok=True)
 
 # Elden Ring installation path
-subprocess.Popen("C:\\Windows\\System32\\calc.exe")
+subprocess.Popen("C:\\Path\\To\\Elden Ring.exe")
 
 print("Task Executed Successfully")
 exit
